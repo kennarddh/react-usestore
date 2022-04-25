fecac1c9683a65c07611506fc5e48c75b7fbea26
kennarddh <kennarddh@gmail.com>
2022-04-25 22:12:26 +0700

    chore: Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { terser } from 'rollup-plugin-terser'
 
 export default {
 	input: 'src/index.js',
-	external: ['react'],
+	external: ['react', 'react-dom'],
 	output: [
 		{
 			file: 'lib/bundle.cjs.js',
