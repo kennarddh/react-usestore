781759e71bea1868afba43ba591139801f449801
kennarddh <kennarddh@gmail.com>
2022-04-25 11:44:55 +0700

    feat: Return store and method from context

--- src/Contexts/Store.jsx
+++ src/Contexts/Store.jsx
@@ -1,9 +1,43 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const StoreContext = createContext({})
 
 const StoreContextProvider = ({ children }) => {
-	return <StoreContext.Provider value={{}}>{children}</StoreContext.Provider>
+	const [Store, SetStore] = useState({})
+
+	const UpdateStore = (storeName, value) => {
+		SetStore({ ...Store, [storeName]: value })
+
+		return true
+	}
+
+	const CreateStore = (storeName, initialValue = {}) => {
+		if (Store[storeName]) return false
+
+		SetStore({ ...Store, [storeName]: initialValue })
+
+		return true
+	}
+
+	const RemoveStore = storeName => {
+		if (!Store[storeName]) return false
+
+		SetStore(prevStore => {
+			const { [storeName]: _, ...result } = prevStore
+
+			return result
+		})
+
+		return true
+	}
+
+	return (
+		<StoreContext.Provider
+			value={{ Store, SetStore, UpdateStore, CreateStore, RemoveStore }}
+		>
+			{children}
+		</StoreContext.Provider>
+	)
 }
 
 export default StoreContextProvider
