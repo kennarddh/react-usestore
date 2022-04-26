4628a9307f2e4db376d31c19651ede626bb6162b
kennarddh <kennarddh@gmail.com>
2022-04-26 11:07:22 +0700

    chore: Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,59 @@ import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 
-export default {
-	input: 'src/index.js',
-	external: ['react', 'react-dom'],
-	output: [
-		{
-			file: 'lib/bundle.cjs.js',
-			format: 'cjs',
-			sourcemap: true,
-		},
-		{
-			file: 'lib/bundle.esm.js',
-			format: 'esm',
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		nodeResolve({
-			extensions: ['.js', '.jsx'],
-		}),
-		commonjs(),
-		babel({
-			exclude: 'node_modules/**',
-			babelHelpers: 'bundled',
-		}),
-		terser(),
-	],
+const EXTENSIONS = ['.js', '.jsx', '.json']
+const CODES = [
+	'THIS_IS_UNDEFINED',
+	'MISSING_GLOBAL_NAME',
+	'CIRCULAR_DEPENDENCY',
+]
+
+const discardWarning = warning => {
+	if (CODES.includes(warning.code)) {
+		return
+	}
+
+	// eslint-disable-next-line no-console
+	console.error(warning)
 }
+
+const commonPlugins = () => [
+	babel({
+		extensions: EXTENSIONS,
+		exclude: 'node_modules/**',
+		babelHelpers: 'bundled',
+	}),
+	commonjs({
+		include: /node_modules/,
+	}),
+	nodeResolve({
+		extensions: EXTENSIONS,
+		preferBuiltins: false,
+	}),
+	terser({
+		output: {
+			comments: false,
+		},
+	}),
+]
+
+export default [
+	{
+		onwarn: discardWarning,
+		external: ['react', 'react-dom'],
+		input: 'src/index.js',
+		output: [
+			{
+				file: 'lib/bundle.cjs.js',
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: 'lib/bundle.esm.js',
+				format: 'esm',
+				sourcemap: true,
+			},
+		],
+		plugins: commonPlugins(),
+	},
+]
